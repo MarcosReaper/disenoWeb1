11b44573904cff0f8c09693efca9f1a0f207f5c8
marcosreaper <marcosreaper@gmail.com>
2018-05-03 12:12:09 -0300

    fix commit

--- proyecto/src/main/java/ar/edu/unlam/tallerweb1/modelo/Farmacia.java
+++ proyecto/src/main/java/ar/edu/unlam/tallerweb1/modelo/Farmacia.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Farmacia {
@@ -14,7 +15,9 @@ public class Farmacia {
 	private String nombre;
 	private String telefono;
 	private String diaDeTurno;
+	@ManyToOne
 	private Direccion direccion;
+	@ManyToOne
 	private Punto geoLocalizacion;
 	public Farmacia() {
 		super();
